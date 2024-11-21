e54f2274db354be18786fda5415dda79aff0d39c
shayann07 <shayankhan17353@gmail.com>
2024-11-21 22:10:13 +0500

    .

--- app/src/main/java/com/shayan/mvvm_login/model/ModelUser.kt
+++ app/src/main/java/com/shayan/mvvm_login/model/ModelUser.kt
@@ -1,10 +1,10 @@
 package com.shayan.mvvm_login.model
 
 data class ModelUser(
-    val firstName: String,    // User's first name
-    val lastName: String,     // User's last name
-    val phone: String,        // User's phone number
-    val password: String,     // User's password
-    val cPass: String,        // Confirm password
+    val firstName: String,    
+    val lastName: String,
+    val phone: String,
+    val password: String,
+    val cPass: String,
     val token: String? = null // Token (only used after successful login)
 )
